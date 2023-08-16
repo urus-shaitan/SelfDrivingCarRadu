fbcf97836801e5c65990e4756813538f54a84270
urus-shaitan <142315269+urus-shaitan@users.noreply.github.com>
2023-08-16 20:37:25 +0000

    Moving road

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(100, 190);
+const car = new Car(road.getLaneCenter(1), 400, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -11,6 +12,10 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.8);
+  road.draw(ctx);
   car.draw(ctx);
+  ctx.restore();
   requestAnimationFrame(animate);
 }
