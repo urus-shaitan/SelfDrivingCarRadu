f9995c7ac1c5827b2e405da4b736ab34b514889e
georg <devnetdot@gmail.com>
2023-08-28 22:31:11 +0300

    Add best car

--- script.js
+++ script.js
@@ -10,32 +10,59 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(100, 190);
-const car = new Car(road.getLaneCenter(1), 400, 30, 50, "AI", 3);
+const N = 500;
+const cars = generateCars(N);
+let bestCar = cars[0];
 
 const traffic = [
-  new Car(road.getLaneCenter(1), 100, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(0), 280, 30, 50, "DUMMY", 2)
+  new Car(road.getLaneCenter(1), 100, 30, 50, "DUMMY", 2.2),
+  new Car(road.getLaneCenter(0), 280, 30, 50, "DUMMY", 2.2)
 ]
 
 animate();
 
+function generateCars(n) {
+  const cars = [];
+  for(let i=0; i<n; i++){
+    cars.push(new Car(road.getLaneCenter(1), 400, 30, 50, "AI", 3));
+  }
+  return cars;
+}
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
+
 function animate(time) {
   traffic.forEach(t => t.update(road.borders, []));
 
-  car.update(road.borders, traffic);
+  cars.forEach(car => car.update(road.borders, traffic));
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
   
+  bestCar = cars.find(car => 
+    car.y === Math.min(...cars.map(c => c.y))
+  );
+
   carCtx.save();
-  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
   road.draw(carCtx);
-  car.draw(carCtx, "blue");
+
+  carCtx.globalAlpha = 0.2;
+  cars.forEach(car => car.draw(carCtx, "blue"));
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue");
+
   traffic.forEach(t => t.draw(carCtx, "red"));
   carCtx.restore();
   
   networkCtx.lineDashOffset =- time/100;
 
-  Visualizer.drawNetwork(networkCtx, car.brain);
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
   requestAnimationFrame(animate);
 }
